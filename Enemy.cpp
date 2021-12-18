5132eeae47b118d4f3b579e561f76cc677e87186
Rosamon <55803598+Rosamon@users.noreply.github.com>
2021-12-18 15:47:50 +0300

    Изменены представления методов. Реализован метод движения объекта

--- Enemy.cpp
+++ Enemy.cpp
@@ -1,10 +1,27 @@
 #include "Enemy.h"
 
-Enemy::Enemy(sf::Vector2f position):
+// 1, 2, 3
+Enemy::Enemy(int position):
 isAlive(true)
 {
-	enemyUFOsprite.setPosition(position);
+	swich (position)
+	{
+		case 1:
+			sf::Vector2f position_UFO(50, 50);
+			brake;
+		case 2:
+			sf::Vector2f position_UFO(300, 50);
+			brake;
+		default:
+			sf::Vector2f position_UFO(500,50);
+			brake;
+	}
+
+	enemyUFOsprite.setPosition(position_UFO);
 	level = 3;
+	Texture enemytexture.loadFromFile("images/Kursa4.png");
+	enemyUFOsprite.setTexture(enemytexture);
+	enemyUFOsprite.setTextureRect(IntRect(416, 45, 73, 48));
 }
 
 void Enemy::punched()
@@ -21,7 +38,48 @@ void Enemy::punched()
 	}
 }
 
-void Enemy::aim()
+void Enemy::aim(sf::Vector2f position)
 {
 	//
+}
+
+bool Enemy::move(float frametime)
+{
+	//
+	sf::Vector2f position = enemyUFOsprite.getposition();
+	if (position_UFO.x >= 500)
+	{
+		direction.x = -1;
+	}
+	if (position_UFO.x <= 50)
+	{
+		direction.x = 1;
+	}
+	if (trunc(position_UFO.x) == 299 || trunc(position_UFO.x) == 300)
+	{
+		short buff = 0;
+		buff = rand() % 2;
+		if (buff == 1)
+		{
+			direction.x = -1;
+		}
+		else
+		{
+			direction.x = 1;
+		}
+	}
+	sf::Vector2f distance = direction * 0.1 * frametime;
+    enemyUFOsprite.move(distance);
+    position_UFO += distance;
+    if (trunc(position_UFO.x) >= 500 || trunc(position_UFO.x) == 299 || trunc(position_UFO.x) == 300 || trunc(position_UFO.x) == 50)
+    {
+    	direction.x = 0;
+    	return false;
+    }
+    return true;
+}
+
+void Enemy::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    target.draw(enemyUFOsprite);
 }
--- Enemy.h
+++ Enemy.h
@@ -5,21 +5,22 @@ class Enemy: public ObjectBase, public sf::Drawable, public sf::Transformable
 public:
 	Enemy(): ObjectBase() {};
 	~Enemy();
-	Enemy(sf::Vector2f position);
+	Enemy(int position);// 1, 2, 3
 	void punched();// окрашивает в красный или белый, считает количество попаданий
-	void punch(bool hit) {isHit = hit;};
+	void punch(bool hit) {ishit = hit;};
     bool checkPoint(sf::Vector2f point);
     void update(float frametime);
-    void draw(sf::RenderTarget& target, sf::RenderStates states);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    bool moved(float frametime);// определяет в движении ли объект
     void aim(sf::Vector2f position); // направляет пулю в сторону ракеты, принимает его координаты, возвращает указатель на созданный класс
     //раз в 3 секунды стрляет time % 3000
-    list <Bullet> shrapnel();// выпускает несколько снарядов под разным углом вниз
+    //list <Bullet> shrapnel();// выпускает несколько снарядов под разным углом вниз
     //
     bool isAlive(){return alive;};
     int getLevel(){return level;};
 private:
 	int level;
-    bool alive, isHit;
+    bool alive, ishit;
     sf::Vector2f direction; // (-1;0) || (1;0)
-    Texture enemyUFOsprite;
+    Sprite enemyUFOsprite;
 };

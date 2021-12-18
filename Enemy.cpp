b8f42be937314f1064a4c2dc459a6a3de2443638
Rosamon <55803598+Rosamon@users.noreply.github.com>
2021-12-18 11:56:00 +0300

    Начало реализации методов класса Enemy

---
+++ Enemy.cpp
@@ -0,0 +1,21 @@
+#include "Enemy.h"
+
+Enemy::Enemy(sf::Vector2f position):
+isAlive(true)
+{
+	enemyUFOsprite.setPosition(position);
+	level = 3;
+}
+void Enemy::punched()
+{
+	if (ishit)
+	{
+		enemyUFOsprite.setColor(Color::Red);
+		level--;
+		ishit = false;
+	}
+	else
+	{
+		enemyUFOsprite.setColor(Color::White);
+	}
+}

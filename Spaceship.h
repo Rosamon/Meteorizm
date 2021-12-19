9e1d3601572d3b93c3b70c988d72852a517411f6
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2021-12-19 20:19:26 +0300

    add variables and functions of sfml

--- Spaceship.h
+++ Spaceship.h
@@ -11,7 +11,11 @@ class Spaceship: public sf::Drawable, public sf::Transformable {
         Spaceship();
         ~Spaceship();
 
-
+  float x, y, w, h, dx, dy, speed = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
+        int dir = 0; //направление (direction) движения игрока
+        Image image;//изображение
+        Texture texture;// текстура
+        Sprite sprite;// спрайт
 
 };
 #endif

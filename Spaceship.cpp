9b23173666254bbda9da0cbc983e55390c07db0f
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2021-12-19 20:23:20 +0300

    and add some other voids

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -49,3 +49,45 @@ void Spaceship::interactionWithMap() {
         }
     }
 }
+void Spaceship::update(float time) {
+    switch (dir)//реализуем поведение в зависимости от направления. (каждая цифра соответствует направлению)
+    {
+    case 0: dx = speed; dy = 0;   break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что ракета летит только вправо
+    case 1: dx = -speed; dy = 0;   break;//по иксу задаем отрицательную скорость, по игреку зануляем. получается, что ракета летит только влево
+    case 2: dx = 0; dy = speed;   break;//по иксу задаем нулевое значение, по игреку положительное. получается, что ракета летит только вниз
+    case 3: dx = 0; dy = -speed;   break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что ракета летит только вверх
+
+    }
+
+    x += dx * time;//то движение из прошлого урока. наше ускорение на время получаем смещение координат и как следствие движение
+    y += dy * time;//аналогично по игреку
+    interactionWithMap();//вызываем функцию, отвечающую за взаимодействие с картой
+    speed = 0;//зануляем скорость, чтобы ракета остановилась.
+    sprite.setPosition(x, y); //выводим спрайт в позицию x y , посередине. бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте
+}
+
+void Spaceship::onEvent(const sf::Event& event) {
+  
+    if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
+
+        dir = 1; speed = 0.2;//dir =1 - направление вверх, speed =0.1 - скорость движения.
+        sprite.setTextureRect(IntRect(319, 155, 68, 66));
+    }
+
+
+    if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
+        dir = 0; speed = 0.2;//направление вправо, см выше
+        sprite.setTextureRect(IntRect(307, 45, 68, 66));
+    }
+
+    if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
+        dir = 3; speed = 0.2;//направление вниз, см выше
+        sprite.setTextureRect(IntRect(238, 38, 45, 73));
+    }
+
+    if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) { //если нажата клавиша стрелка влево или англ буква А
+        dir = 2; speed = 0.2;//направление вверх, см выше
+        sprite.setTextureRect(IntRect(238, 38, 45, 73));
+    }
+
+}

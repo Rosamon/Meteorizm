3b02766e4b836186019165e52b0a14e5f926a96b
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2021-12-19 20:18:03 +0300

    Begining of spaceship header

--- Spaceship.h
+++ Spaceship.h
@@ -1,18 +1,14 @@
-#ifndef _SPACESHIP_H_
-#define _SPACESHIP_H_
+#ifndef _SPACESHIP_HPP_
+#define _SPACESHIP_HPP_
 
 
 #include <SFML/Graphics.hpp>
+#include "Constants.h"
 using namespace sf;
-class SpaceShip { // класс Игрока 
-public:
-    float x, y, w, h, dx, dy, speed = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
-    int dir = 0; //направление (direction) движения игрока
-    String File; //файл с картинкой
-    Image image;//изображение
-    Texture texture;// текстура
-    Sprite sprite;// спрайт
-    SpaceShip(String F, float X, float Y, float W, float H);
+class Spaceship: public sf::Drawable, public sf::Transformable {
+
+
+
 
 };
 #endif

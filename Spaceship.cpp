cae08d8aa2cdf25b0eba2e03d8abc484e51c5521
DmitryYakin <93880312+DmitryYakin@users.noreply.github.com>
2021-12-19 21:53:08 +0300

    пытаюсь выжить

--- Spaceship.cpp
+++ Spaceship.cpp
@@ -6,8 +6,10 @@ Spaceship::Spaceship() {
     image.loadFromFile("images\\ship.png" );//запихиваем в image наше изображение вместо File мы передадим то, что пропишем при создании объекта.
     texture.loadFromImage(image);//закидываем наше изображение в текстуру
     sprite.setTexture(texture);//заливаем спрайт текстурой
-    x = 475; y = 915;//координата появления спрайта
     sprite.setTextureRect(IntRect(238, 38, 45, 73));  //Задаем спрайту один прямоугольник для вывода одного корабля. IntRect - приведение типов
+    x = 475; y = 915;//координата появления спрайта
+    speed = 1;
+    reset();
 }
 
 Spaceship::~Spaceship() {
@@ -15,6 +17,7 @@ Spaceship::~Spaceship() {
 
 void Spaceship::reset() {
     setPosition(475, 915);
+    rotate(-90);
 }
 
 void Spaceship::draw(sf::RenderTarget& target, sf::RenderStates states) const {
@@ -49,44 +52,67 @@ void Spaceship::interactionWithMap() {
         }
     }
 }
-void Spaceship::update(float time) {
+
+void Spaceship::update(float frametime) {
     switch (dir)//реализуем поведение в зависимости от направления. (каждая цифра соответствует направлению)
     {
-    case 0: dx = speed; dy = 0;   break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что ракета летит только вправо
-    case 1: dx = -speed; dy = 0;   break;//по иксу задаем отрицательную скорость, по игреку зануляем. получается, что ракета летит только влево
-    case 2: dx = 0; dy = speed;   break;//по иксу задаем нулевое значение, по игреку положительное. получается, что ракета летит только вниз
-    case 3: dx = 0; dy = -speed;   break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что ракета летит только вверх
+    case 0: dx = spee; dy = 0;   break;//по иксу задаем положительную скорость, по игреку зануляем. получаем, что ракета летит только вправо
+    case 1: dx = -spee; dy = 0;   break;//по иксу задаем отрицательную скорость, по игреку зануляем. получается, что ракета летит только влево
+    case 2: dx = 0; dy = spee;   break;//по иксу задаем нулевое значение, по игреку положительное. получается, что ракета летит только вниз
+    case 3: dx = 0; dy = -spee;   break;//по иксу задаем нулевое значение, по игреку отрицательное. получается, что ракета летит только вверх
 
     }
 
-    x += dx * time;//то движение из прошлого урока. наше ускорение на время получаем смещение координат и как следствие движение
-    y += dy * time;//аналогично по игреку
+    x += dx * frametime;//то движение из прошлого урока. наше ускорение на время получаем смещение координат и как следствие движение
+    y += dy * frametime;//аналогично по игреку
     interactionWithMap();//вызываем функцию, отвечающую за взаимодействие с картой
-    speed = 0;//зануляем скорость, чтобы ракета остановилась.
+    spee = 0; //зануляем скорость, чтобы ракета остановилась.
     sprite.setPosition(x, y); //выводим спрайт в позицию x y , посередине. бесконечно выводим в этой функции, иначе бы наш спрайт стоял на месте
+    if (dy != 0 ||dx != 0) {
+
+        sf::Vector2f distance;
+        distance.x += dx * frametime * speed;
+        distance.y += dy * frametime * speed;
+
+        move(distance);
+        sprite.move(distance);
+    }
+    spee = 0;
+    sf::Vector2f position = getPosition();
+    if (position.x < -10.0f)
+        position.x = APP_WIDTH;
+    else if (position.x > APP_WIDTH)
+        position.x = 0.0f;
+
+    if (position.y < -10.0f)
+        position.y = APP_HEIGHT;
+    else if (position.y > APP_HEIGHT)
+        position.y = 0.0f;
+
+    setPosition(position);
 }
 
 void Spaceship::onEvent(const sf::Event& event) {
   
     if ((Keyboard::isKeyPressed(Keyboard::Left) || (Keyboard::isKeyPressed(Keyboard::A)))) {
 
-        dir = 1; speed = 0.2;//dir =1 - направление вверх, speed =0.1 - скорость движения.
+        dir = 1; spee = 0.2;//dir =1 - направление вверх, speed =0.1 - скорость движения.
         sprite.setTextureRect(IntRect(319, 155, 68, 66));
     }
 
 
     if ((Keyboard::isKeyPressed(Keyboard::Right) || (Keyboard::isKeyPressed(Keyboard::D)))) {
-        dir = 0; speed = 0.2;//направление вправо, см выше
+        dir = 0; spee= 0.2;//направление вправо, см выше
         sprite.setTextureRect(IntRect(307, 45, 68, 66));
     }
 
     if ((Keyboard::isKeyPressed(Keyboard::Up) || (Keyboard::isKeyPressed(Keyboard::W)))) {
-        dir = 3; speed = 0.2;//направление вниз, см выше
+        dir = 3; spee = 0.2;//направление вниз, см выше
         sprite.setTextureRect(IntRect(238, 38, 45, 73));
     }
 
     if ((Keyboard::isKeyPressed(Keyboard::Down) || (Keyboard::isKeyPressed(Keyboard::S)))) { //если нажата клавиша стрелка влево или англ буква А
-        dir = 2; speed = 0.2;//направление вверх, см выше
+        dir = 2; spee = 0.2;//направление вверх, см выше
         sprite.setTextureRect(IntRect(238, 38, 45, 73));
     }
 
--- Spaceship.h
+++ Spaceship.h
@@ -1,26 +1,28 @@
 #ifndef _SPACESHIP_HPP_
 #define _SPACESHIP_HPP_
 
-
+#include <math.h>
 #include <SFML/Graphics.hpp>
-#include "Constants.h"
+#include "Constants.hpp"
 using namespace sf;
 class Spaceship: public sf::Drawable, public sf::Transformable {
+
 
     public:
         Spaceship();
         ~Spaceship();
-
-  float x, y, w, h, dx, dy, speed = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
+       
+        float x, y, w, h, dx, dy,speed, spee = 0; //координаты игрока х и у, высота ширина, ускорение (по х и по у), сама скорость
         int dir = 0; //направление (direction) движения игрока
         Image image;//изображение
         Texture texture;// текстура
         Sprite sprite;// спрайт
-         void reset();
-        void update(float time);
+        void reset();
+        void update(float frametime);
         void interactionWithMap();
         void draw(sf::RenderTarget& target, sf::RenderStates states) const;
         void onEvent(const sf::Event& event);
+        sf::Vector2f position;
 
 
 };

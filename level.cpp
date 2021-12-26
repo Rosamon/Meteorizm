9d8f34db636e44275b21c8e07282dd6a7e0cab7e
Foantik <nasta3187@gmail.com>
2021-12-26 13:43:43 +0300

    Добавила счёт и столкновение с астероидами в .cpp

--- level.cpp
+++ level.cpp
@@ -1,6 +1,15 @@
 #include "Level.h"
-#include <iostream>
+#include <sstream>
 Level::Level() {
+    font.loadFromFile("images\\CyrilicOld.ttf");
+    text.setFont(font);
+    //text.setColor(Color::Red);//покрасили текст в красный. если убрать эту строку, то по умолчанию он белый
+    text.setCharacterSize(24);
+    text.setStyle(sf::Text::Bold | sf::Text::Underlined);
+    text.setPosition(20,20);
+
+    Score = 0;
+
     for (int i=0; i < 3; i++) {
         Asteroid a(0);///
         asteroids.push_back(a);
@@ -13,38 +22,51 @@ Level::~Level() {
 void Level::onEvent(const sf::Event& event) {
     ship.onEvent(event);
 
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Space)) {
+    
+}
+bool Level::update(float frametime) {
+    ship.update(frametime);
+    
+    ShipTimer += frametime;//
+    UFOAimTimer += frametime;//
+    UFOSpawnTimer += frametime;//
+    AsteroidSpawnTimer += frametime;//
+
+    if (ShipTimer > 300)///////////////
+    {
         Bullet bullet(ship.getPosition(), ship.getRotation(), false);
         bullets.push_back(bullet);
-        
+        ShipTimer = 0;
     }
-}
-bool Level::update(float frametime, int my_timer) {
-    ship.update(frametime);
     if (UFO != nullptr)
     {
         UFO->update(frametime);
     }
     /////////////////
-    if ((my_timer % 300) == 0 && UFO != nullptr && UFO->isAlive())///////////////
+
+
+    if (UFOAimTimer > 500 && UFO != nullptr && UFO->isAlive())
     {
-        bullets.push_back(UFO->aim(ship.getPosition()));/////////////
-       
+        bullets.push_back(UFO->aim(ship.getPosition()));
+
+        UFOAimTimer = 0;
     }
-    if ((my_timer % 3000 == 0 || my_timer % 3000 == 1) && UFO == nullptr)
+    if ((UFOSpawnTimer > 30000) && UFO == nullptr)
     {
-        UFO = new Enemy(2);  
+        UFO = new Enemy((rand() % 3 + 1));  
+        UFOSpawnTimer = 0;
     }
     if (UFO != nullptr && !UFO->isAlive())
     {
+        Score += 5;
         delete UFO;
         UFO = nullptr;
     }
-    if ((my_timer % 700) == 0 && UFO != nullptr && UFO->isAlive())///////////////
+    if (AsteroidSpawnTimer > 1500)///////////////
     {
         Asteroid a(0);///
         asteroids.push_back(a);
-
+        AsteroidSpawnTimer = 0;
     }
     //Работа класса enemy
     ////////////////
@@ -60,13 +82,16 @@ bool Level::update(float frametime, int my_timer) {
     std::vector<Asteroid>::iterator start_asteroids = asteroids.begin();
     while (start_asteroids != asteroids.end()) {
         if (start_asteroids->isAlive()) {
+            if (ship.isСollision(start_asteroids->getTouchBox())) {//Столкновение
+                start_asteroids->breakDown();//
+                Score++;
+            }
             start_asteroids->update();
             ++start_asteroids;
         } else
             start_asteroids = asteroids.erase(start_asteroids);
     }
 
-    std::vector<Asteroid> new_asteroids;
     start_asteroids = asteroids.begin();
 
     while (start_asteroids != asteroids.end()) {
@@ -76,21 +101,13 @@ bool Level::update(float frametime, int my_timer) {
                 ++start_bullets;
                 continue;
             }
-
             if (start_asteroids->checkPoint(start_bullets->getPosition())){
                 start_bullets->kill();
                 start_asteroids->breakDown();
-
-                if (start_asteroids->isAlive()) {
-                    sf::Vector2f position = start_asteroids->getPosition();
-                    //float angle = rand() % 360;
-                    //Asteroid a(position, angle, start_asteroids->getLevel());
-                    //Asteroid a(0);
-                    //new_asteroids.push_back(a);
-                }
+                if(!start_bullets->isItEnemy())
+                Score++;
                 break;
                 ////
-                
             }
             ++start_bullets;
         }
@@ -102,8 +119,7 @@ bool Level::update(float frametime, int my_timer) {
             if (ship.checkPoint(start_bullets->getPosition()))
             {
                 start_bullets->kill();
-                if (ship.dedinside())
-                    return false;
+                
             }
             ++start_bullets;
             continue;
@@ -117,28 +133,34 @@ bool Level::update(float frametime, int my_timer) {
         ////////////////     
         ++start_bullets;
         }
-        
+    if (ship.dedinside())
+        return false;
     
-    asteroids.insert(asteroids.end(), new_asteroids.begin(), new_asteroids.end());
+    //asteroids.insert(asteroids.end(), new_asteroids.begin(), new_asteroids.end());
     return true;
 }
 
 void Level::show(sf::RenderTarget& target) {
-    target.draw(ship);
-
-    //
-    if (UFO != nullptr && UFO->isAlive())
-    {
-        sf::RenderStates states;
-           UFO->draw(target, states);
-    }
-    // Отрисовка тарелки
-    ///////////
+    
     for(std::vector<Bullet>::iterator it = bullets.begin(); it != bullets.end(); ++it)
         target.draw(*it);
 
+    target.draw(ship);
+
     for(std::vector<Asteroid>::iterator it = asteroids.begin(); it != asteroids.end(); ++it)
         target.draw(*it);
+
+    if (UFO != nullptr && UFO->isAlive())
+    {
+        sf::RenderStates states;
+        UFO->draw(target, states);
+    }
+    
+    std::ostringstream playerScoreString;    // объявили переменную
+    playerScoreString << Score;
+    text.setString("Score: " + playerScoreString.str());
+    target.draw(text);
+    
 }
 
 void Level::start() {

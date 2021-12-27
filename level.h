d3d5c983feb84a6da8d70a23960defd404730a0d
Foantik <nasta3187@gmail.com>
2021-12-27 18:26:19 +0300

    Добавила комментарии

--- level.h
+++ level.h
@@ -1,31 +1,31 @@
-#ifndef _LEVEL_H_
+#ifndef _LEVEL_H_ //Если не объявлен level.h, то объявить его.
 #define _LEVEL_H_
 
-#include "Enemy.h"
+#include "Enemy.h" // Подключаем библиотеки
 //#include "Bullet.h"
 #include <SFML/Graphics.hpp>
 #include "Asteroids.h"
 #include "Spaceship.h"//пока нет
 
 
-class Level {
-    public:
-        Level();
-        ~Level();
-        void onEvent(const sf::Event& event);
-        bool update(float frametime);
-        void show(sf::RenderTarget& target);
+class Level { // Объявляем класс
+    public: // Методы
+        Level();//Конструктор
+        ~Level();//Деструктор
+        void onEvent(const sf::Event& event);//Обработка событий
+        bool update(float frametime);//Обновление картинок
+        void show(sf::RenderTarget& target);//Показ объекта
         void start();
 
-    private:
-        Spaceship ship;
-        std::vector<Bullet> bullets;
-        std::vector<Asteroid> asteroids;
-        Enemy* UFO;
-        float ShipTimer, UFOAimTimer, UFOSpawnTimer, AsteroidSpawnTimer = 0;
-        Font font;
-        Text text;
-        int Score;
+    private://Поля(атрибуты)
+        Spaceship ship;//Корабль типа класса spaceship
+        std::vector<Bullet> bullets;//Вектор(список) пуль
+        std::vector<Asteroid> asteroids;//Вектор(список) астероидов
+        Enemy* UFO;//НЛО добавил Рома
+        float ShipTimer, UFOAimTimer, UFOSpawnTimer, AsteroidSpawnTimer = 0;//Таймеры
+        Font font;//Фон
+        Text text;//Текст
+        int Score;//Очки игрока
 
 };
 
